1bb92fe3fa77d5690f11d5c91e0446d1a5f76234
BeeMOre32 <windowssart01@gmail.com>
2022-07-28 16:43:23 +0900

    update

---
+++ src/theme.ts
@@ -0,0 +1,9 @@
+import { DefaultTheme } from "styled-components";
+
+export const DarkTheme: DefaultTheme = {
+  bodyColor: "#1f2937",
+  textColor: "black",
+  cardColor: "white",
+  accentColor: "#2ecc71",
+  bgColor: "#34495e",
+};
